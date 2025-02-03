93c97c2a72183cf0968f3a0a1737f7f48eb554eb
corycastaneda14 <ccastaneda176@students.mchenry.edu>
2025-02-03 12:47:40 -0600

    Comments on another code file.
    These are four comments I made on a code file.

---
+++ comments.py
@@ -0,0 +1,10 @@
+"""This is for comments and testing out the comments that I create on this code.
+    This is the summary for the code below.
+"""
+tank_size = 15
+# Define the total distance covered
+distance = 600#this is the distance in miles
+# Calculate miles per gallon
+mpg = distance / tank_size#I assume the tank size is in feet
+# Print the result
+print("Miles per gallon:", mpg)#This is the result of the division in line 9
