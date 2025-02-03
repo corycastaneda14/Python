961cfb5a89e74e550ee972c9726af6251c378843
corycastaneda14 <ccastaneda176@students.mchenry.edu>
2025-02-03 12:40:17 -0600

    Kilograms to Pounds Conversion Calculator
    Kilograms to Pounds Conversion Calculator

---
+++ kilograms.py
@@ -0,0 +1,16 @@
+#This converts kilograms to pounds
+kg_value_1 = 15
+kg_value_2 = 22
+kg_value_3 = 28
+kg_value_4 = 33
+#The Conversion Factor is 1 kilogram = 2.20462 pounds
+conversion_factor = 2.20462
+pounds_1 = kg_value_1*conversion_factor
+pounds_2 = kg_value_2*conversion_factor
+pounds_3 = kg_value_3*conversion_factor
+Pounds_4 = kg_value_4*conversion_factor
+#These are the results.
+print(f"{kg_value_1} kilograms is equal to {pounds_1} pounds.")
+print(f"{kg_value_2} kilograms is equal to {pounds_2} pounds.")
+print(f"{kg_value_3} kilograms is equal to {pounds_3} pounds.")
+print(f"{kg_value_4} kilograms is equal to {Pounds_4} pounds.")
