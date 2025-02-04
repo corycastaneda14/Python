8aae8b0520dff2583718162f2af2109afdbb9eeb
corycastaneda14 <ccastaneda176@students.mchenry.edu>
2025-02-03 18:57:57 -0600

    Python Data Types
    These are the different data types in Python.

---
+++ datatypes.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 21)
+
+# Integer
+y = 70
+print("Integer:", y)
+
+# Float
+z = 27.2
+print("Float:", z)
+
+# String
+name = "Cory"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
